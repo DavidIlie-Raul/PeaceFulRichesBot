6a186ce866546d70ecdcee3c9196d1c91074d226
PeacefulRiches <peacefulricheshost@gmail.com>
2023-09-09 17:21:58 +0200

    replaced globalfetch with axios so it works on vps as well

--- src/commands/tools/quote.js
+++ src/commands/tools/quote.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,20 +7,35 @@ module.exports = {
     .setDescription("Displays a random Quote"),
 
   async execute(interaction, client) {
-    function getQuote() {
-      return fetch("https://zenquotes.io/api/random")
-        .then((res) => res.json())
-        .then((data) => data[0]["q"] + "-" + data[0]["a"])
-        .catch((error) => {
-          console.log("An error has occurred:", error);
-          return "An error has occured with supplying your quote!, please try again later!";
-          // Handle the error here, such as providing a fallback quote or taking alternative actions
-        });
+    const axios = require("axios");
+
+    async function getQuote() {
+      try {
+        console.log("Fetching quote...");
+        const response = await axios.get("https://zenquotes.io/api/random");
+        const data = response.data;
+        console.log("Received data from API:", data);
+
+        if (data && data.length > 0) {
+          return data[0]["q"] + "-" + data[0]["a"];
+        } else {
+          throw new Error("No data received from the API");
+        }
+      } catch (error) {
+        console.error("An error has occurred:", error);
+        throw new Error(
+          "An error has occurred with supplying your quote!, please try again later!"
+        );
+      }
     }
+
     try {
+      console.log("Attempting to get a quote...");
       const quote = await getQuote();
+      console.log("Received quote:", quote);
       await interaction.reply(quote);
     } catch (error) {
+      console.error("Error while getting quote:", error);
       await interaction.reply({
         content:
           "Sorry, An error has occurred with displaying a quote, please try again later!",
